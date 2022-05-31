fb90b2806b24b202d46fba7d2d7c635de10c5011
Francael <fransozzan@gmail.com>
2022-05-31 15:24:22 -0300

    [skills] todos los logos

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,14 +1,56 @@
 import { FunctionComponent } from "react"
+import Image from "next/image"
+import css from "../styles/Skills.module.css"
+
+const Item: FunctionComponent<{url:string, name:string}> = ({url, name}) => {
+	return <div className={css.item}>
+		<img src={url} height={30} style={{maxWidth:'100%'}} />
+		<p>{name}</p>
+	</div>
+}
 
 const Skills: FunctionComponent = () => {
-	return <>
-		<ul>
-			<li>Habilidad 1</li>
-			<li>Habilidad 2</li>
-			<li>Habilidad 3</li>
-			<li>Habilidad 4</li>
-		</ul>
-	</>
+	return <div className={`el ${css.skillsScreen}`}>
+		<h4>Langs</h4>
+		<div className={css.list}>
+			<Item url="/js.svg" name="Javascript"/> 
+			<Item url="/ts.svg" name="Typescript" />
+			<Item url="/python.svg" name="Python" />
+			<Item url="/html.svg" name="HTML" />
+			<Item url="/css.svg" name="CSS"/>
+		</div>
+		<h4>Front-end</h4>
+		<div className={css.list}>
+			<Item url="/react.svg" name="Reactjs"/>
+			<Item url="/nextjs.svg" name="Nextjs"/>
+			<Item url="/solidjs.svg" name="Solidjs"/>
+			<Item url="/vuejs.svg" name="Vuejs"/>
+			<Item url="/threejs.svg" name="Threejs"/>
+		</div>
+		<h4>Back-end</h4>
+		<div className={css.list}>
+			<Item url="/nodejs.svg" name="Nodejs" />
+			<Item url="/expressjs.svg" name="Expressjs" />
+			<Item url="/socketio.svg" name="Websockets" />
+			<Item url="/api.svg" name="RestAPI" />
+		</div>
+		<h4>Databases</h4>
+		<div className={css.list}>
+			<Item url="/firebase.svg" name="Firebase" />
+			<Item url="/mongodb.svg" name="MongoDB" />
+			<Item url="/redis.svg" name="Redis" />
+		</div>
+		<h4>Tools</h4>
+		<div className={css.list}>
+			<Item url="/git.svg" name="Git" />
+			<Item url="/figma.svg" name="Figma" />
+			<Item url="/docker.svg" name="Docker" />
+		</div>
+		<h4>Learning</h4>
+		<div className={css.list}>
+			<Item url="/rust.svg" name="Rust" />
+		</div>
+	</div>
 }
 
 export default Skills
