0e5e7f4286e6c290963a4276f4e41192edbee062
Francael <fransozzan@gmail.com>
2022-05-31 09:56:10 -0300

    [feedback] preparado para api

--- components/Feedback.tsx
+++ components/Feedback.tsx
@@ -1,14 +1,28 @@
-import { FunctionComponent } from "react"
-import styles from '../styles/Component.module.css'
+import { FunctionComponent, useRef } from "react"
+import css from '../styles/Component.module.css'
 
-const Feedback: FunctionComponent = () => {
-	return <>
+const Feedback:FunctionComponent = () => {
+	const nameField = useRef<HTMLInputElement>(null)
+	const textField = useRef<HTMLTextAreaElement>(null)
+
+	const submit = () => {
+		if (!nameField.current || !textField.current) return
+		if (!textField.current.value) return alert('Write an opinion')
+		
+		const feedback = {
+			name: nameField.current.value,
+			text: textField.current.value
+		}
+		// ship this feedback to my api
+	}
+
+	return <div className="el">
 		<p>Name (optional)</p>
-		<input type="text" maxLength={30}/>
+		<input type="text" maxLength={30} ref={nameField} />
 		<p>Your opinion</p>
-		<textarea></textarea>
-		<button className={styles.formSubmit}>Send</button>
-	</>
+		<textarea ref={textField} ></textarea>
+		<button className={css.formSubmit} onClick={submit}>Send</button>
+	</div>
 }
 
 export default Feedback
